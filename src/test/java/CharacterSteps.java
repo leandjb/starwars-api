88443cb00b378826907433ebace7bf8aa497ed5e
leandjb <leandjb@gmail.com>
2023-06-02 22:49:31 -0500

    Initial commit

---
+++ src/test/java/CharacterSteps.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CharacterSteps {
+    @Given("^send a GET request to https://swapi\\.dev/api/people/(\\d+)/$")
+    public void sendAGETRequestToHttpsSwapiDevApiPeople() {
+
+
+    }
+
+    @Then("^obtain (\\d+) status code$")
+    public void obtainStatusCode() {
+
+
+    }
+
+}
